bf4e0b91624f5b3779d9909b4095303e8a64c81a
Diana <5275194+DianaNites@users.noreply.github.com>
2020-07-12 01:31:01 -0400

    Header writing stuff

--- src/new/header.rs
+++ src/new/header.rs
@@ -1,5 +1,5 @@
 //! GPT Header stuff
-use core::convert::TryFrom;
+use core::{convert::TryFrom, mem, slice};
 use crc::{crc32, Hasher32};
 use displaydoc::Display;
 use thiserror::Error;
@@ -15,10 +15,19 @@ pub struct Invalid(&'static str);
 const SIGNATURE: [u8; 8] = *b"EFI PART";
 
 /// Current/supported GPT Header revision
-const _REVISION: u32 = 0x0001_0000;
+const REVISION: u32 = 0x0001_0000;
 
 /// Minimum header size. Anything smaller than this is invalid.
-pub const MIN_HEADER_SIZE: u32 = 92;
+const MIN_HEADER_SIZE: u32 = 92;
+
+/// Partition Entry Size.
+const PARTITION_ENTRY_SIZE: u32 = 128;
+
+/// A minimum of 16,384 bytes are reserved for the partition array.
+///
+/// With current GPT Partition entry sizes(128) this means a minimum of 128
+/// partitions
+const MIN_PARTITION_BYTES: u64 = 16384;
 
 /// Start of the GPT Header Structure
 ///
@@ -56,8 +65,24 @@ impl HeaderPre {
     }
 }
 
+/// Header kind
+pub enum HeaderKind {
+    Primary,
+    Backup,
+}
+
+/// Default partition alignment
+pub enum Align {
+    /// Align to 1 MiB boundaries.
+    Optimal,
+
+    /// Align to the bare minimum
+    Minimal,
+}
+
 /// GPT Header, revision 1.0
 #[repr(C, packed)]
+#[cfg_attr(test, derive(Debug, Copy, Clone, PartialEq))]
 pub struct Header {
     /// Hard-coded to [`SIGNATURE`]
     signature: [u8; 8],
@@ -109,6 +134,53 @@ pub struct Header {
 }
 
 impl Header {
+    // TODO: Make a HeaderBuilder instead?
+    pub fn new(
+        kind: HeaderKind,
+        align: Align,
+        partitions_crc32: u32,
+        partitions: u32,
+        disk_guid: Uuid,
+        disk_size: u64,
+        block_size: u64,
+    ) -> Self {
+        let array_lba_size = MIN_PARTITION_BYTES / block_size;
+        // Minus 1 because LBA starts at zero.
+        let last = (disk_size / block_size) - 1;
+        // Last, minus array blocks, minus 1 for the header.
+        let last_usable_lba = last - array_lba_size - 1;
+        let alt = last;
+        Header {
+            signature: SIGNATURE,
+            revision: REVISION,
+            header_size: MIN_HEADER_SIZE,
+            header_crc32: 0,
+            _reserved: 0,
+            this_lba: match kind {
+                HeaderKind::Primary => 1,
+                HeaderKind::Backup => alt,
+            },
+            alt_lba: match kind {
+                HeaderKind::Backup => 1,
+                HeaderKind::Primary => alt,
+            },
+            first_usable_lba: match align {
+                Align::Optimal => 2048,
+                // Blocks for the partition array, and 2 for the MBR and header.
+                Align::Minimal => array_lba_size + 2,
+            },
+            last_usable_lba,
+            disk_guid,
+            partition_array_start: match kind {
+                HeaderKind::Primary => 2,
+                HeaderKind::Backup => last_usable_lba + 1,
+            },
+            partitions,
+            partition_size: PARTITION_ENTRY_SIZE,
+            partitions_crc32,
+        }
+    }
+
     /// Read the GPT Header from `source`,
     /// located at the logical block address `lba`.
     ///
@@ -136,6 +208,37 @@ impl Header {
         let header = unsafe { &*(source.as_ptr() as *const Header) };
         header.validate(source, lba);
         header
+    }
+
+    /// Write the GPT header to `dest`.
+    ///
+    /// This takes `&mut self` because it never makes sense to
+    /// write out a `&self` from [`Header::read`],
+    /// which can't have changed by definition.
+    ///
+    /// # Panics
+    ///
+    /// - If `dest` is not `MIN_HEADER_SIZE`
+    pub fn write(&mut self, dest: &mut [u8], _block_size: u64) {
+        assert_eq!(
+            dest.len(),
+            MIN_HEADER_SIZE as usize,
+            "BUG: Dest must be `MIN_HEADER_SIZE` bytes"
+        );
+        let mut digest = crc32::Digest::new(crc32::IEEE);
+        // SAFETY:
+        // - `self` is valid and aligned.
+        // - `Header` is `repr(C, packed)`
+        // - `self` is not mutated
+        let raw = unsafe {
+            let ptr = self as *mut Header as *mut u8;
+            slice::from_raw_parts_mut(ptr, mem::size_of::<Header>())
+        };
+        digest.write(raw);
+        // Set header_crc32. Not through `self` assignment since
+        // `raw` is live, so it's UB.
+        raw[16..][..4].copy_from_slice(&digest.sum32().to_le_bytes());
+        dest.copy_from_slice(raw);
     }
 
     /// UEFI UUID's are stored in a mixed endian format,
@@ -193,6 +296,7 @@ mod tests {
         },
         *,
     };
+    use pretty_assertions::assert_eq;
     use static_assertions::*;
 
     assert_eq_size!(HeaderPre, [u8; 16]);
@@ -211,6 +315,38 @@ mod tests {
                 "UUID didn't match test data"
             );
             assert_eq!(header.uuid().variant(), uuid::Variant::Rfc4122);
+        }
+        Ok(())
+    }
+
+    /// Should be able to round-trip correctly
+    #[test]
+    fn roundtrip() -> Result<()> {
+        for data in TEST_DATA {
+            let source_bytes = &data.bytes[MBR_SIZE..][..MIN_HEADER_SIZE as usize];
+            let source = Header::read(source_bytes, data.block_size, 1);
+            let mut bytes = [0u8; MIN_HEADER_SIZE as usize];
+            let mut new = Header::new(
+                HeaderKind::Primary,
+                // Parted and cfdisk use different defaults.
+                if source.first_usable_lba == 34 {
+                    Align::Minimal
+                } else {
+                    Align::Optimal
+                },
+                source.partitions_crc32,
+                source.partitions,
+                source.disk_guid,
+                data.bytes.len() as u64,
+                data.block_size,
+            );
+            new.write(&mut bytes, data.block_size);
+            assert_eq!(source, Header::read(&bytes, data.block_size, 1));
+            assert_eq!(
+                &source_bytes[..],
+                &bytes[..],
+                "Written bytes did not match read bytes"
+            );
         }
         Ok(())
     }
